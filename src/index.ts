e18d6bcf277c8968f61a783a265f82cee2244295
Alexey Kolpakov <al.kolpak@gmail.com>
2019-10-04 00:03:30 +0300

    initial

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 // method decorator @sync_timer
 export function sync_timer(target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
     if (propertyDescriptor === undefined) {
-        propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
+        propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey)!;
     }
+    const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
     const originalMethod = propertyDescriptor.value;
     propertyDescriptor.value = function(...args: any[]) {
         const t0 = new Date().valueOf();
-        console.log(`[timer] [${target.constructor.name}::${propertyKey}]: begin`);
-        const result = originalMethod.apply(this, args);
-        console.log(`[timer] [${target.constructor.name}::${propertyKey}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
-        return result;
+        console.log(`[timer] [${timername}]: begin`);
+        try {
+            const result = originalMethod.apply(this, args);
+            console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
+            return result;
+        } catch (err) {
+            console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
+            throw err;
+        }
     };
     return propertyDescriptor;
 }
@@ -17,18 +23,19 @@ export function sync_timer(target: any, propertyKey: string, propertyDescriptor:
 // method decorator @async_timer
 export function async_timer(target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
     if (propertyDescriptor === undefined) {
-        propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
+        propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey)!;
     }
+    const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
     const originalMethod = propertyDescriptor.value;
     propertyDescriptor.value = async function(...args: any[]) {
         const t0 = new Date().valueOf();
-        console.log(`[timer] [${target.constructor.name}::${propertyKey}]: begin`);
+        console.log(`[timer] [${timername}]: begin`);
         try {
             const result = await originalMethod.apply(this, args);
-            console.log(`[timer] [${target.constructor.name}::${propertyKey}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
+            console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
             return result;
         } catch (err) {
-            console.log(`[timer] [${target.constructor.name}::${propertyKey}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
+            console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
             throw err;
         }
     };
