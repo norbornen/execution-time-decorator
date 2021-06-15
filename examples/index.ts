8b6f63ccfea4f08cd1fc410b7bbdc9ae634fcfd2
Alexey Kolpakov <al.kolpak@gmail.com>
2021-06-16 02:34:04 +0300

    bump packages

--- examples/index.ts
+++ examples/index.ts
@@ -4,72 +4,68 @@ import * as fs from 'fs';
 import * as crypto from 'crypto';
 
 class ExampleTimers {
+  @sync_timer
+  readSync(filepath: string) {
+    return fs.readFileSync(filepath);
+  }
 
-    @sync_timer
-    public readSync(filepath: string) {
-        return fs.readFileSync(filepath);
-    }
+  @sync_timer
+  readSyncThrow(filepath: string) {
+    return fs.readFileSync(filepath + 'a');
+  }
 
-    @sync_timer
-    public readSyncThrow(filepath: string) {
-        return fs.readFileSync(filepath + 'a');
-    }
+  @async_timer
+  async readAsync(filepath: string) {
+    return fs.promises.readFile(filepath);
+  }
 
-    @async_timer
-    public async readAsync(filepath: string) {
-        return fs.promises.readFile(filepath);
-    }
+  @async_timer
+  async readAsyncThrow(filepath: string) {
+    return fs.promises.readFile(filepath + 'a');
+  }
 
-    @async_timer
-    public async readAsyncThrow(filepath: string) {
-        return fs.promises.readFile(filepath + 'a');
-    }
+  @sync_hrtimer
+  static readSync(filepath: string) {
+    return fs.readFileSync(filepath);
+  }
 
-    @sync_hrtimer
-    public static readSync(filepath: string) {
-        fs.readFileSync(filepath);
-    }
-
-    @async_hrtimer
-    public static async readAsync(filepath: string) {
-        return fs.promises.readFile(filepath);
-    }
-
+  @async_hrtimer
+  static async readAsync(filepath: string) {
+    return fs.promises.readFile(filepath);
+  }
 }
 
 (async () => {
+  const data = await promisify(crypto.randomBytes)(50 * 1024 * 1024);
+  const filepath = `${__dirname}/example.data`;
+  await fs.promises.writeFile(filepath, data);
 
-    const data = await promisify(crypto.randomBytes)(50 * 1024 * 1024);
-    const filepath = `${__dirname}/example.data`;
-    await fs.promises.writeFile(filepath, data);
+  let t0 = new Date().valueOf();
+  await promisify(fs.readFile)(filepath);
+  console.log(`read promisify(fs.readFile) ${(new Date().valueOf() - t0) * 0.001}`);
+  t0 = new Date().valueOf();
+  await fs.promises.readFile(filepath);
+  console.log(`read fs.promises.readFile ${(new Date().valueOf() - t0) * 0.001}`);
+  console.log('=====================================================');
 
-    let t0 = new Date().valueOf();
-    await promisify(fs.readFile)(filepath);
-    console.log(`read promisify(fs.readFile) ${ (new Date().valueOf() - t0) * 0.001 }`);
-    t0 = new Date().valueOf();
-    await fs.promises.readFile(filepath);
-    console.log(`read fs.promises.readFile ${ (new Date().valueOf() - t0) * 0.001 }`);
-    console.log('=====================================================');
+  const example = new ExampleTimers();
+  await t(() => example.readSync(filepath));
+  await t(() => example.readSyncThrow(filepath));
+  await t(() => example.readAsync(filepath));
+  await t(() => example.readAsyncThrow(filepath));
+  await t(() => ExampleTimers.readSync(filepath));
+  await t(() => ExampleTimers.readAsync(filepath));
 
-    const example = new ExampleTimers();
-    await t(() => example.readSync(filepath));
-    await t(() => example.readSyncThrow(filepath));
-    await t(() => example.readAsync(filepath));
-    await t(() => example.readAsyncThrow(filepath));
-    await t(() => ExampleTimers.readSync(filepath));
-    await t(() => ExampleTimers.readAsync(filepath));
-
-    await fs.promises.unlink(filepath);
-
+  await fs.promises.unlink(filepath);
 })();
 
-async function t(fn: () => any): Promise<void> {
-    try {
-        const res = fn();
-        if (res instanceof Promise) {
-            await res;
-        }
-    } catch (err) {
-        // console.error((err as Error).message);
+async function t(fn: () => PromiseLike<Buffer> | Buffer): Promise<void> {
+  try {
+    const res = fn();
+    if (res instanceof Promise) {
+      await res;
     }
+  } catch (err) {
+    // console.error((err as Error).message);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,95 @@
-export function sync_timer(target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
-    if (propertyDescriptor === undefined) {
-        propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey)!;
+export function sync_timer(
+  target: any,
+  propertyKey: string,
+  propertyDescriptor: PropertyDescriptor
+): PropertyDescriptor {
+  propertyDescriptor = propertyDescriptor || Object.getOwnPropertyDescriptor(target, propertyKey);
+
+  const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
+  const originalMethod = propertyDescriptor.value;
+  propertyDescriptor.value = function (...args: any[]) {
+    const t0 = new Date().valueOf();
+    console.log(`[timer] [${timername}]: begin`);
+    try {
+      const result = originalMethod.apply(this, args);
+      console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
+      return result;
+    } catch (err) {
+      console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
+      throw err;
     }
-    const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
-    const originalMethod = propertyDescriptor.value;
-    propertyDescriptor.value = function(...args: any[]) {
-        const t0 = new Date().valueOf();
-        console.log(`[timer] [${timername}]: begin`);
-        try {
-            const result = originalMethod.apply(this, args);
-            console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
-            return result;
-        } catch (err) {
-            console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
-            throw err;
-        }
-    };
-    return propertyDescriptor;
+  };
+  return propertyDescriptor;
 }
 
-export function async_timer(target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
-    if (propertyDescriptor === undefined) {
-        propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey)!;
+export function async_timer(
+  target: any,
+  propertyKey: string,
+  propertyDescriptor: PropertyDescriptor
+): PropertyDescriptor {
+  propertyDescriptor = propertyDescriptor || Object.getOwnPropertyDescriptor(target, propertyKey);
+
+  const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
+  const originalMethod = propertyDescriptor.value;
+  propertyDescriptor.value = async function (...args: any[]) {
+    const t0 = new Date().valueOf();
+    console.log(`[timer] [${timername}]: begin`);
+    try {
+      const result = await originalMethod.apply(this, args);
+      console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
+      return result;
+    } catch (err) {
+      console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
+      throw err;
     }
-    const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
-    const originalMethod = propertyDescriptor.value;
-    propertyDescriptor.value = async function(...args: any[]) {
-        const t0 = new Date().valueOf();
-        console.log(`[timer] [${timername}]: begin`);
-        try {
-            const result = await originalMethod.apply(this, args);
-            console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
-            return result;
-        } catch (err) {
-            console.log(`[timer] [${timername}]: timer ${((new Date().valueOf() - t0) * 0.001).toFixed(3)}s`);
-            throw err;
-        }
-    };
-    return propertyDescriptor;
+  };
+  return propertyDescriptor;
 }
 
-export function sync_hrtimer(target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
-    if (propertyDescriptor === undefined) {
-        propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey)!;
+export function sync_hrtimer(
+  target: any,
+  propertyKey: string,
+  propertyDescriptor: PropertyDescriptor
+): PropertyDescriptor {
+  propertyDescriptor = propertyDescriptor || Object.getOwnPropertyDescriptor(target, propertyKey);
+
+  const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
+  const originalMethod = propertyDescriptor.value;
+  propertyDescriptor.value = function (...args: any[]) {
+    const t0 = process.hrtime.bigint();
+    console.log(`[hrtimer] [${timername}]: begin`);
+    try {
+      const result = originalMethod.apply(this, args);
+      console.log(`[hrtimer] [${timername}]: timer ${(process.hrtime.bigint() - t0)}ns`);
+      return result;
+    } catch (err) {
+      console.log(`[hrtimer] [${timername}]: timer ${(process.hrtime.bigint() - t0)}ns`);
+      throw err;
     }
-    const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
-    const originalMethod = propertyDescriptor.value;
-    propertyDescriptor.value = function(...args: any[]) {
-        const t0 = process.hrtime.bigint();
-        console.log(`[hrtimer] [${timername}]: begin`);
-        try {
-            const result = originalMethod.apply(this, args);
-            console.log(`[hrtimer] [${timername}]: timer ${(process.hrtime.bigint() - t0)}ns`);
-            return result;
-        } catch (err) {
-            console.log(`[hrtimer] [${timername}]: timer ${(process.hrtime.bigint() - t0)}ns`);
-            throw err;
-        }
-    };
-    return propertyDescriptor;
+  };
+  return propertyDescriptor;
 }
 
-export function async_hrtimer(target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
-    if (propertyDescriptor === undefined) {
-        propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey)!;
+export function async_hrtimer(
+  target: any,
+  propertyKey: string,
+  propertyDescriptor: PropertyDescriptor
+): PropertyDescriptor {
+  propertyDescriptor = propertyDescriptor || Object.getOwnPropertyDescriptor(target, propertyKey);
+
+  const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
+  const originalMethod = propertyDescriptor.value;
+  propertyDescriptor.value = async function (...args: any[]) {
+    const t0 = process.hrtime.bigint();
+    console.log(`[hrtimer] [${timername}]: begin`);
+    try {
+      const result = await originalMethod.apply(this, args);
+      console.log(`[hrtimer] [${timername}]: timer ${(process.hrtime.bigint() - t0)}ns`);
+      return result;
+    } catch (err) {
+      console.log(`[hrtimer] [${timername}]: timer ${(process.hrtime.bigint() - t0)}ns`);
+      throw err;
     }
-    const timername = (target instanceof Function ? `static ${target.name}` : target.constructor.name) + `::${propertyKey}`;
-    const originalMethod = propertyDescriptor.value;
-    propertyDescriptor.value = async function(...args: any[]) {
-        const t0 = process.hrtime.bigint();
-        console.log(`[hrtimer] [${timername}]: begin`);
-        try {
-            const result = await originalMethod.apply(this, args);
-            console.log(`[hrtimer] [${timername}]: timer ${(process.hrtime.bigint() - t0)}ns`);
-            return result;
-        } catch (err) {
-            console.log(`[hrtimer] [${timername}]: timer ${(process.hrtime.bigint() - t0)}ns`);
-            throw err;
-        }
-    };
-    return propertyDescriptor;
+  };
+  return propertyDescriptor;
 }
